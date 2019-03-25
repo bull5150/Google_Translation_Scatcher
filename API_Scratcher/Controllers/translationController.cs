6a802eddb125d4b9cf42cba2c6a32784f5cd5c10
Heldridge, Jeffrey M <Jeffrey.Heldridge@tdameritrade.com>
2019-03-25 15:07:09 -0500

    Inital Commit

---
+++ API_Scratcher/Controllers/translationController.cs
@@ -0,0 +1,34 @@
+﻿using Google.Cloud.Translation.V2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API_Scratcher.Controllers
+{
+    [RoutePrefix("api/transtester")]
+    public class translationController : ApiController
+    {
+        /// <summary>
+        /// Input text for input field, and then add langauge code for what language you want back
+        /// </summary>
+        /// <param name="inputvalue"></param>
+        /// <param name="language_code"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("")]
+        public string GetTranslation(string inputvalue, string language_code)
+        {
+            //documentation here: https://cloud.google.com/translate/docs/quickstart?csw=1
+            //enable api here maybe: https://console.cloud.google.com/apis/api/translate.googleapis.com/overview?project=curious-song-235615&folder&organizationId
+            string credential_path = @"FilePath\googlekey.json";
+            System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credential_path);
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            TranslationClient client = TranslationClient.Create();
+            var response = client.TranslateText(inputvalue, language_code);
+            return response.TranslatedText;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Google_Translation_Scatcher
-Just a simple API service to use the Google Translation Service
+# Google Translation Service
+I built this using my TempAPI project and then added the Google services needed, detailed below.
+
+ - Create a free trail on the Google Cloud https://console.cloud.google.com/
+ - Next create an app on the Google Cloud under your account
+ - Enable Translation API for your app https://console.cloud.google.com/apis/dashboard?project
+ - create service account key and download it
+ - reference key in the translation controller
+ 
